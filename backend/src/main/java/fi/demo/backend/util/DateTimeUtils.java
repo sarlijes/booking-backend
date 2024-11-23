a067a9ff54453a06adb8f90c4af0376637e68cf7
Jessica Sarlin <jessica.sarlin@gmail.com>
2024-11-23 20:51:43 +0200

    Refine javadoc

--- backend/src/main/java/fi/demo/backend/util/DateTimeUtils.java
+++ backend/src/main/java/fi/demo/backend/util/DateTimeUtils.java
@@ -7,11 +7,12 @@ public class DateTimeUtils {
 
     /**
      * Returns whether a given datetime of interest will occur during a given period of time, counting from
-     * a start given datetime.
-     * @param start
-     * @param thresholdPeriod
-     * @param dateTimeOfInterest
-     * @return
+     * a start given datetime. When the two given datetimes are equal, method will return true.
+     * @param start: DateTime when the period will start
+     * @param thresholdPeriod: the threshold Period.
+     * @param dateTimeOfInterest: the datetime for which the method will evaluate, whether it's during
+     *                          the given period, starting from the given start -datetime.
+     * @return boolean
      */
     public static boolean isDuring(LocalDateTime start, Period thresholdPeriod, LocalDateTime dateTimeOfInterest) {
         return dateTimeOfInterest.isEqual(start.plus(thresholdPeriod))
